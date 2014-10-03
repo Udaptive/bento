1d864949bf1213337758658323fd5832347c870c
John Rinderle <john@acrobatiq.com>
2014-10-02 23:48:31 -0400

    debugging volume initialization

--- packer/scripts/platform/volumes-all_in_one.sh
+++ packer/scripts/platform/volumes-all_in_one.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-mke4fs -t ext4 /dev/sdc
+mke2fs -t ext4 /dev/xvdc
 echo '/dev/xvdc /vagrant ext4 defaults,noatime 0 2' >> /etc/fstab
 mkdir /vagrant
 mount /vagrant
 
-mke4fs -t ext4 /dev/sdd
+mke2fs -t ext4 /dev/xvdd
 echo '/dev/xvdd /data ext4 defaults,noatime 0 2' >> /etc/fstab
 mkdir /data
 mount /data
 
-mke4fs -t ext4 /dev/sde
+mke2fs -t ext4 /dev/xvde
 echo '/dev/xvdd /project_repos ext4 defaults,noatime 0 2' >> /etc/fstab
 mkdir /project_repos
 mount /project_repos
